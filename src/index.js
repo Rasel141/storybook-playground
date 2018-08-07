c295403143afd1beaf07d8c53fd339f26d7487c5
Rasel141 <rrasel141@gmail.com>
2018-08-07 14:03:23 +0600

    Remove -unnecessary components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as AppMine } from './components/AppMine'
 export { default as AppMineStyledCom } from './components/AppMineStyledCom'
-export { MojaButton } from './components/MojaButton'
 export { SearchBar } from './components/SearchBar'
--- stories/index.js
+++ stories/index.js
@@ -1,31 +1,22 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { Button, Welcome } from '@storybook/react/demo'
-import styled from 'styled-components'
-import AppMine from '../src/components/AppMine'
-import AppMineStyledCom from '../src/components/AppMineStyledCom'
-import { MojaButton } from '../src/components/MojaButton'
-import { SearchBar } from '../src/components/SearchBar'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+// import { action } from "@storybook/addon-actions";
+// import { Button, Welcome } from "@storybook/react/demo";
+import { SearchBar } from "../src/components/SearchBar";
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role='img' aria-label='so cool'>😀 😎 👍 💯</span>
-    </Button>
-  ))
+storiesOf("Button", module)
+  // .add("with text", () => (
+  //   <Button onClick={action("clicked")}>Hello Button</Button>
+  // ))
+  // .add("with some emoji", () => (
+  //   <Button onClick={action("clicked")}>
+  //     <span role="img" aria-label="so cool">
+  //       😀 😎 👍 💯
+  //     </span>
+  //   </Button>
+  // ));
 
-storiesOf('AppMine', module).add('default', () => <AppMine />)
-
-storiesOf('AppMineStyledCom', module).add('default', () => <AppMineStyledCom />)
-
-storiesOf('Mojar Button', module).add('Mojar Button', () => (
-  <MojaButton>LOL</MojaButton>
-))
-
-storiesOf('Search Bar', module).add('Search Bar', () => (
-  <SearchBar/>
-))
+storiesOf("Input Field", module)
+  .add("Search Bar", () => 
+    <SearchBar />
+  );
